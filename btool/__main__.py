a34656f0cc5df530187a5e504c5d0de719f963ef
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2022-04-09 15:09:37 -0400

    macos nonsense, I'd like permission to read my app's stdout/err please

--- btool/__main__.py
+++ btool/__main__.py
@@ -10,6 +10,8 @@ import subprocess
 import shutil
 import traceback
 import platform
+import threading
+import select
 
 def is_windows_host():
   return os.name == 'nt'
@@ -64,17 +66,43 @@ sproc = subprocess.Popen(server_cmd, cwd=os.path.join('.'))
 
 try:
   if is_macos_host():
+    stdout_fifo = os.path.abspath( os.path.join('target', 'htir_app_stdout.fifo') )
+    stderr_fifo = os.path.abspath( os.path.join('target', 'htir_app_stderr.fifo') )
+    if not os.path.exists(stdout_fifo):
+      os.mkfifo(stdout_fifo)
+    if not os.path.exists(stderr_fifo):
+      os.mkfifo(stderr_fifo)
+
+    # Spawn a thread to poll each FIFO object
+    os.environ['KILL_POLL_FIFO_T'] = 'f'
+    def poll_fifo_write_to(fifo_file, out):
+      with open(fifo_file, 'r') as fd:
+        while not 't' in os.environ.get('KILL_POLL_FIFO_T', ''):
+          select.select([fd],[],[fd]) # Wait until I/O available
+          data = fd.read()
+          if len(data) > 0:
+            out.write(data)
+
+
+    t1 = threading.Thread(target=poll_fifo_write_to, args=(stdout_fifo, sys.stdout))
+    t1.start()
+    t2 = threading.Thread(target=poll_fifo_write_to, args=(stderr_fifo, sys.stderr))
+    t2.start()
+
     client_cmd = [
       '/usr/bin/open',
       '-W', # Wait for app to close
-      '--stdin', '/dev/stdin',       # Forward stdin
-      '--stdout', '/dev/stdout',     # Forward stdout
-      '--stderr', '/dev/stderr',     # Forward stderr
+      '--stdin', '/dev/stdin',     # Forward stdin
+      '--stdout', stdout_fifo,     # Forward stdout (causes permission errors if /dev/stdout)
+      '--stderr', stderr_fifo,     # Forward stderr (causes permission errors if /dev/stderr)
       '-a', HTIR_app, # -a <application>.app
       '--args'] + list(sys.argv[1:])
 
     print('Running MacOS client app: {}'.format(' '.join(client_cmd)))
     subprocess.run(client_cmd, cwd=os.path.join('.'))
+    os.environ['KILL_POLL_FIFO_T'] = 'true'
+    time.sleep(0.1)
+
   else:
     client_cmd = [client_exe] + list(sys.argv[1:])
     print('Running client command: {}'.format(' '.join(client_cmd)))
