7d0205af1d3633aa07d90685d09ea2b1e040ff38
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2022-04-09 14:06:22 -0400

    macos API experiments

--- src/gui/gui_macos.rs
+++ src/gui/gui_macos.rs
@@ -18,9 +18,17 @@ struct BasicApp {
 
 impl AppDelegate for BasicApp {
     fn did_finish_launching(&self) {
+        println!("BasicApp.did_finish_launching()");
         self.window.set_minimum_content_size(400., 400.);
         self.window.set_title("Hello World!");
         self.window.show();
+    }
+    fn will_terminate(&self) {
+      println!("BasicApp.will_terminate()");
+    }
+    fn should_terminate_after_last_window_closed(&self) -> bool {
+      println!("BasicApp.should_terminate_after_last_window_closed()");
+      return true;
     }
 }
 
