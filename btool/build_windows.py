376e468047084c45a5dfb19e735b12b63c7269dc
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2022-04-11 20:03:11 -0400

    swap around some statements so theyre true more often

--- btool/build_windows.py
+++ btool/build_windows.py
@@ -22,17 +22,22 @@ def build_all():
   )
 
   if utils.is_x64_host():
-    if utils.is_linux_host():
-      subprocess.run(['cargo', 'build', '--release', '--target', 'x86_64-pc-windows-gnu'], check=True)
-    else:
+    if utils.is_windows_host():
+      # Windows host are assumed to have MSVC available
       subprocess.run(['cargo', 'build', '--release', '--target', 'x86_64-pc-windows-msvc'], check=True)
-
+      
+    else:
+      # Everyone else is assumed to use gnu tools
+      subprocess.run(['cargo', 'build', '--release', '--target', 'x86_64-pc-windows-gnu'], check=True)
+      
   elif utils.is_aarch64_host():
-    if utils.is_linux_host():
+    if utils.is_windows_host():
+      subprocess.run(['cargo', 'build', '--release', '--target', 'aarch64-pc-windows-msvc'], check=True)
+
+    else:
       #subprocess.run(['cargo', 'build', '--release', '--target', 'aarch64-pc-windows-gnu'], check=True)
       raise Exception('GNU does not publish anything for the aarch64-pc-windows-gnu target triple!')
-    else:
-      subprocess.run(['cargo', 'build', '--release', '--target', 'aarch64-pc-windows-msvc'], check=True)
+
 
   else:
     raise Exception('Unknown host CPU type!')
