c7ac742bafc195d836ab688b4aeec054074e248f
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2022-04-11 20:14:09 -0400

    changes to cross compile configs

--- btool/build_common_pre_exe.py
+++ btool/build_common_pre_exe.py
@@ -16,6 +16,8 @@ from . import utils
 
 def build_all():
   create_misc_directories_we_assume_exist()
+
+  create_cargo_config_if_necessary()
   
   # Uses a 3rd-party renderer & python code to render high-quality .png and generate a .icns file under ./target/
   icon_gen.gen_icons(os.path.abspath(os.path.join('htir_app_icon.pov')))
@@ -27,6 +29,20 @@ def create_misc_directories_we_assume_exist():
   
   os.makedirs('target', exist_ok=True)
 
+def create_cargo_config_if_necessary():
+  if utils.is_linux_host():
+    os.makedirs('.cargo', exist_ok=True)
+    with open(os.path.join('.cargo', 'config'), 'w+') as fd:
+      fd.write('''
+# We assume the following is installed: https://github.com/tpoechtrager/osxcross
+[target.x86_64-apple-darwin]
+linker = "o64-clang"
+ar = "x86_64-apple-darwin20.4-ar"
 
+# We assume the following is installed: https://www.mingw-w64.org/
+[target.x86_64-pc-windows-gnu]
+linker = "x86_64-w64-mingw32-gcc"
+ar = "x86_64-w64-mingw32-ar"
+'''.strip()+'\n')
 
 
