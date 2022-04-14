64459133628520ee433acf782dfe1a8696c6610a
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2022-04-14 16:18:03 -0400

    the _proper_ way to give all OSes opt-in to GTK graphics

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -9,22 +9,22 @@
 
 // Use win32 graphics if windows & native UI enabled
 
-#[cfg(all(target_os = "windows"))]
+#[cfg(all(target_os = "windows", not(feature = "force_gtk_ui")))]
 pub mod gui_win32;
-#[cfg(all(target_os = "windows"))]
+#[cfg(all(target_os = "windows", not(feature = "force_gtk_ui")))]
 pub use gui_win32 as gui;
 
 // Use cocoa graphics if macos & native UI enabled
 
-#[cfg(all(target_os = "macos"))]
+#[cfg(all(target_os = "macos", not(feature = "force_gtk_ui")))]
 pub mod gui_macos;
-#[cfg(all(target_os = "macos"))]
+#[cfg(all(target_os = "macos", not(feature = "force_gtk_ui")))]
 pub use gui_macos as gui;
 
 // Use GTK windows if feature=native_ui not specified OR we are compiling for a unix variant.
 
-#[cfg(any(target_os = "linux", target_os = "freebsd", target_os = "openbsd", target_os = "netbsd"))]
+#[cfg(any(target_os = "linux", target_os = "freebsd", target_os = "openbsd", target_os = "netbsd", feature = "force_gtk_ui"))]
 pub mod gui_unix_gtk;
-#[cfg(any(target_os = "linux", target_os = "freebsd", target_os = "openbsd", target_os = "netbsd"))]
+#[cfg(any(target_os = "linux", target_os = "freebsd", target_os = "openbsd", target_os = "netbsd", feature = "force_gtk_ui"))]
 pub use gui_unix_gtk as gui;
 
