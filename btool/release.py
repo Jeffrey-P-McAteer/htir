f2ab857f586f76ac226c43c868d58bd72b492eb5
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2022-04-13 20:59:47 -0400

    release test

--- btool/release.py
+++ btool/release.py
@@ -7,6 +7,7 @@ import subprocess
 # Our libraries
 
 import btool
+from . import utils
 
 def main(args=sys.argv):
   btool.main(args)
@@ -52,17 +53,28 @@ def main(args=sys.argv):
   if len(github_username) < 1:
     raise Exception('Cannot upload release, GITHUB_USERNAME={}'.format(github_username))
 
+  git_porcelain_out = subprocess.check_output(['git', 'status', '--porcelain'], stderr=subprocess.STDOUT)
+  git_porcelain_out = git_porcelain_out.strip()
+  if len(git_porcelain_out) > 2:
+    raise Exception('Refusing to release when current directory is not committed! git_porcelain_out={}'.format(git_porcelain_out))
+
   github_binary_upload = utils.import_maybe_installing_with_pip('github_binary_upload', pkg_name='github-binary-upload')
   toml = utils.import_maybe_installing_with_pip('toml')
   cargo_toml = toml.load('Cargo.toml')
   release_version = cargo_toml.get('package', {}).get('version', '0.0.0')
+  release_version = 'v{}'.format(release_version)
+
+  # Create version tag & push to remote, ignoring errors.
+  print('Creating tag {}'.format(release_version))
+  subprocess.run(['git', 'tag', release_version], check=False)
+  subprocess.run(['git', 'push', 'origin', release_version], check=False)
 
   print('Releasing version {}'.format(release_version))
   for f in assets_to_upload:
     print('> {}'.format(os.path.basename(f)))
 
   github_binary_upload.publish_release_from_tag(
-    'htir', None, assets_to_upload, 'https://api.github.com', github_username, github_access_token, False
+    '{}/htir'.format(github_username), None, assets_to_upload, 'github.com', github_username, github_access_token, False
   )
 
   
