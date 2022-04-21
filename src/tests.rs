7293ab68982d6a6d421824b5dbedc227786b2bca
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2022-04-20 20:24:37 -0400

    testing + docs

--- src/tests.rs
+++ src/tests.rs
@@ -2,17 +2,22 @@
 use crate::*;
 
 #[test]
-fn gen_config() {
+fn config_memory() {
     let mut c = config::Config::default();
 
     c.enrich_server();
     c.enrich_client();
 
     let socket = c.get_tcp_listen_socket();
-    
-
 
 }
 
+
+#[test]
+fn config_empty_file() {
+    let mut c = config::read_config(Some("/dev/null")); // TODO make test work on windorks as well
+    
+
+}
 
 
